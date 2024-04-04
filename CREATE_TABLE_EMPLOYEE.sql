fee82892846024a15f89ea7f3c4d35c2eca4a52f
ibrahimcansunovax <fujitsunovax@hotmail.com>
2024-04-04 15:26:23 +0300

    Create employee table

---
+++ CREATE_TABLE_EMPLOYEE.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Employee (
+    Employee_Id INT IDENTITY(1,1) PRIMARY KEY,
+    Start_Date DATE,
+    End_Date DATE,
+    Title VARCHAR(50),
+    Name VARCHAR(50),
+    Surname VARCHAR(50),
+    Fullname AS Name + ' ' + Surname
+);
